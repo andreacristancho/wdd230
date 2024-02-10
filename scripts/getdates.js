1d90c3704b68551ed8e00b9ca02020ae30cbcd44
andreacristancho <andreacristancho01@gmail.com>
2024-02-09 19:25:21 -0500

    Home Page Andrea Feb 9_24  7 25pm

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Copyright information
-    const copyrightInfo = document.getElementById("copyrightInfo");
-    const currentYear = new Date().getFullYear();
-    copyrightInfo.textContent = `© ${currentYear} - Andrea Cristancho - Colombia`;
-  
-    // Last modified information
-    const lastModified = document.getElementById("lastModified");
-    const lastModifiedDate = document.lastModified;
-    lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
-  });
-  
+  // Copyright information
+  const copyrightInfo = document.getElementById("copyrightInfo");
+  const currentYear = new Date().getFullYear();
+  copyrightInfo.textContent = `© ${currentYear} - Andrea Cristancho - Colombia`;
+
+  // Last modified information
+  const lastModified = document.getElementById("lastModified");
+  const lastModifiedDate = document.lastModified;
+  lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
+});
+
+
+const navigation = document.querySelector(".navigation");
+const hamButton = document.querySelector("#menu");
+
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("show");
+  hamButton.classList.toggle("open");
+});
+
