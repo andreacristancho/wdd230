bd9369071ee1cf0871d6411fe5a5c27a104b624f
andreacristancho <andreacristancho01@gmail.com>
2024-02-17 14:07:56 -0500

    Chamber site changes 17 feb 2pm

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Copyright information
-    const copyrightInfo = document.getElementById("copyrightInfo");
-    const currentYear = new Date().getFullYear();
-    copyrightInfo.textContent = `© ${currentYear} - Andrea Cristancho - Colombia`;
-  
-    // Last modified information
-    const lastModified = document.getElementById("lastModified");
-    const lastModifiedDate = document.lastModified;
-    lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
-  });
-  
+  // Copyright information
+  const copyrightInfo = document.getElementById("copyrightInfo");
+  const currentYear = new Date().getFullYear();
+  copyrightInfo.textContent = `© ${currentYear} - Andrea Cristancho - Colombia`;
+
+  // Last modified information
+  const lastModified = document.getElementById("lastModified");
+  const lastModifiedDate = document.lastModified;
+  lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
+});
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
