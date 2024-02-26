efd221a1ca2901124275ff36cf66167ddbe35f63
andreacristancho <andreacristancho01@gmail.com>
2024-02-25 22:14:33 -0500

    Menu Hamburguer changes Feb 25_24 10 15pm

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -10,13 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
   lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
 });
 
-
-const navigation = document.querySelector(".navigation");
+const mainnav = document.querySelector(".navigation");
 const hamButton = document.querySelector("#menu");
 
-
 hamButton.addEventListener("click", () => {
-  navigation.classList.toggle("show");
+  mainnav.classList.toggle("show");
   hamButton.classList.toggle("open");
 });
-
